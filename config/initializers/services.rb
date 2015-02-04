47c151e950a2b85beacbc83e81000679a6ee1575
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-02-04 15:08:22 +0000

    Add services registry.

---
+++ config/initializers/services.rb
@@ -0,0 +1,22 @@
+module PolicyPublisher
+  def self.services(name, service = nil)
+    @services ||= {}
+
+    if service
+      @services[name] = service
+      return true
+    else
+      if @services[name]
+        return @services[name]
+      else
+        raise ServiceNotRegisteredException.new(name)
+      end
+    end
+  end
+
+  class ServiceNotRegisteredException < Exception; end
+end
+
+## EXAMPLE
+# require 'gds_api/publishing_api'
+# CollectionsPublisher.services(:publishing_api, GdsApi::PublishingApi.new(Plek.new.find('publishing-api')))
