eaf139c86d57a1165e778f43b2bd5f24397f5778
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2015-02-05 12:30:07 +0000

    Quick /healthcheck endpoint and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :policies
 
+  get "/healthcheck" => Proc.new { [200, {}, ["OK"]] }
+
   root to: "policies#index"
 end
---
+++ spec/requests/healthcheck_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "the healthcheck page", :type => :request do
+  it "should return success" do
+    get "/healthcheck"
+
+    expect(response.status).to eq(200)
+  end
+end
