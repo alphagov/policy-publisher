665543ce52b8f57525fba3888c1f4c9d3300b5a5
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-02-10 13:18:06 +0000

    Validate presence of policy names.

--- spec/models/policy_spec.rb
+++ spec/models/policy_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Policy do
 
     expect(policy.slug).to eq("climate-change")
   end
+
+  it "doesn't permit blank names" do
+    blank_policy = FactoryGirl.build(:policy, name: '')
+    nil_policy = FactoryGirl.build(:policy, name: nil)
+
+    expect(blank_policy).not_to be_valid
+    expect(nil_policy).not_to be_valid
+  end
 end
