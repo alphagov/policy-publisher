c323d59798402b93b1dd6ba7428a74d7d081bb10
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-02-11 15:34:25 +0000

    Add ContentId to PolicyArea
    In order to publish a Finder for each PolicyArea we need to have a
    content_id for each PolicyArea. This commit adds a migration to add this
    to the table and generates this on create. It also enforces a uniqueness
    validation in the model and table.

--- app/models/policy_area.rb
+++ app/models/policy_area.rb
@@ -1,8 +1,10 @@
 class PolicyArea < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
+  validates :content_id, presence: true, uniqueness: true
 
   before_validation on: :create do |policy_area|
     policy_area.slug = policy_area.name.to_s.parameterize
+    policy_area.content_id = SecureRandom.uuid
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211151548) do
+ActiveRecord::Schema.define(version: 20150211152824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20150211151548) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "content_id"
   end
 
+  add_index "policy_areas", ["content_id"], name: "index_policy_areas_on_content_id", unique: true, using: :btree
   add_index "policy_areas", ["name"], name: "index_policy_areas_on_name", unique: true, using: :btree
   add_index "policy_areas", ["slug"], name: "index_policy_areas_on_slug", unique: true, using: :btree
 
