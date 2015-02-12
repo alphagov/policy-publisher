b74d61d7fc4074def58895a6819da8e302b7367f
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-02-12 15:01:03 +0000

    Publish a PolicyArea Content Item after save
    This commit changes the PolicyArea model to publish a Content Item after
    save. It also updates the tests to reflect the change and stub out the
    Publishing API.

--- app/models/policy_area.rb
+++ app/models/policy_area.rb
@@ -1,3 +1,5 @@
+require 'gds_api/publishing_api'
+
 class PolicyArea < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
@@ -6,5 +8,17 @@ class PolicyArea < ActiveRecord::Base
   before_validation on: :create do |policy_area|
     policy_area.slug = policy_area.name.to_s.parameterize
     policy_area.content_id = SecureRandom.uuid
+  end
+
+  after_save :publish_policy_area_finder
+
+private
+  def publish_policy_area_finder
+    attrs = PolicyAreaContentItemPresenter.new(self).exportable_attributes
+    publishing_api.put_content_item(attrs["base_path"], attrs)
+  end
+
+  def publishing_api
+    @publishing_api || PolicyPublisher.services(:publishing_api)
   end
 end
--- features/policy_areas.feature
+++ features/policy_areas.feature
@@ -7,8 +7,10 @@ In order to inform the nation about our government's intentions for a topic.
 Scenario: Creating a policy area
   When I create a policy area called "Climate change"
   Then there should be a policy area called "Climate change"
+  Then a policy area called "Climate change" is published "1" times
 
 Scenario: Editing a policy area
-  Given a policy area exists called "Global warming"
+  Given a published policy area exists called "Global warming"
   When I change the title of policy area "Global warming" to "Climate change"
   Then there should be a policy area called "Climate change"
+  Then a policy area called "Global warming" is published "2" times
