1a00c2f4accd14232ff6753d4b04c9f3cca198cb
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-02-13 11:38:17 +0000

    Refactor publishing out to a concern
    This code will be reused for a Programme so it makes sense to refactor
    it out to a concern.

---
+++ app/models/concerns/publishable.rb
@@ -0,0 +1,26 @@
+require 'gds_api/publishing_api'
+
+module Publishable
+  extend ActiveSupport::Concern
+
+  included do
+    validates :content_id, presence: true, uniqueness: true
+
+    before_validation on: :create do |object|
+      object.slug = object.name.to_s.parameterize
+      object.content_id = SecureRandom.uuid
+    end
+
+    after_save :publish_finder
+  end
+
+private
+  def publish_finder
+    attrs = PolicyAreaContentItemPresenter.new(self).exportable_attributes
+    publishing_api.put_content_item(attrs["base_path"], attrs)
+  end
+
+  def publishing_api
+    @publishing_api ||= PolicyPublisher.services(:publishing_api)
+  end
+end
