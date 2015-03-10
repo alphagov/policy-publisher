8dfbe42589a99f968bd4b161398d7ee862718edb
Tekin Suleyman <tekin@tekin.co.uk>
2015-03-10 15:22:12 +0000

    Test presentation of both an area and a programme

--- spec/presenters/content_item_presenter_spec.rb
+++ spec/presenters/content_item_presenter_spec.rb
@@ -8,17 +8,16 @@ RSpec.describe ContentItemPresenter do
     stub_default_publishing_api_put
   end
 
-  let(:policy) {
-    PolicyArea.create!(
-      name: "Tea and biscuits",
-      description: "Tea and biscuits are popular among many people who live in the UK.",
-    )
-  }
-
-  let(:presenter) { ContentItemPresenter.new(policy) }
-
   describe "#exportable_attributes" do
-    it "validates against the schema for a policy" do
+    it "validates against the schema with a policy area" do
+      presenter = ContentItemPresenter.new(FactoryGirl.create(:policy_area))
+
+      assert_valid_against_schema(presenter.exportable_attributes.as_json, "policy")
+    end
+
+    it "validates against the schema with a policy programme" do
+      presenter = ContentItemPresenter.new(FactoryGirl.create(:programme))
+
       assert_valid_against_schema(presenter.exportable_attributes.as_json, "policy")
     end
   end
