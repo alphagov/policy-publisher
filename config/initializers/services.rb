1bb275e58df1c5bf95ddebce4b946e41ee2c356e
Tekin Suleyman <tekin@tekin.co.uk>
2015-03-11 16:04:56 +0000

    Separate service registering from getting
    Instead of doing two things with one method, have separate methods for each
    thing.

--- config/initializers/services.rb
+++ config/initializers/services.rb
@@ -3,25 +3,19 @@ require 'gds_api/content_register'
 require 'gds_api/rummager'
 
 module PolicyPublisher
-  def self.services(name, service = nil)
+  def self.register_service(name, service)
     @services ||= {}
 
-    if service
-      @services[name] = service
-      return true
-    else
-      if @services[name]
-        return @services[name]
-      else
-        raise ServiceNotRegisteredException.new(name)
-      end
-    end
+    @services[name] = service
+  end
+
+  def self.services(name)
+    @services[name] or raise ServiceNotRegisteredException.new(name)
   end
 
   class ServiceNotRegisteredException < Exception; end
 end
 
-
-PolicyPublisher.services(:publishing_api, GdsApi::PublishingApi.new(Plek.new.find('publishing-api')))
-PolicyPublisher.services(:rummager, GdsApi::Rummager.new(Plek.new.find('search')))
-PolicyPublisher.services(:content_register, GdsApi::ContentRegister.new(Plek.new.find('content-register')))
+PolicyPublisher.register_service(:publishing_api, GdsApi::PublishingApi.new(Plek.new.find('publishing-api')))
+PolicyPublisher.register_service(:rummager, GdsApi::Rummager.new(Plek.new.find('search')))
+PolicyPublisher.register_service(:content_register, GdsApi::ContentRegister.new(Plek.new.find('content-register')))
