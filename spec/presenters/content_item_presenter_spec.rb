41f29ba4d92a5e9366051d9673499cf12500ecb1
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-03-12 16:27:53 +0000

    Applicable nations in Content Item
    This commit adds applicable nations to the content item if applicable
    nations is less than possible nations. The data structure is as follows:
    
    ```
    "nation_applicability": {
      "applies_to": [
        "england",
        "northern_ireland"
      ],
      "alternative_policies": [
        {
          "nation": "scotland",
          "alt_policy_url": "http://gov.scot"
        },
        {
          "nation": "wales",
          "alt_policy_url": "http://gov.wales/"
        }
       ]
    }
    ```

--- spec/presenters/content_item_presenter_spec.rb
+++ spec/presenters/content_item_presenter_spec.rb
@@ -47,5 +47,44 @@ RSpec.describe ContentItemPresenter do
 
       expect(attributes["links"]["people"]).to eq([content_id])
     end
+
+    it "doesn't add nation_applicability if the policy applies to all nations" do
+      policy_area = FactoryGirl.create(:policy_area)
+      attributes = ContentItemPresenter.new(policy_area).exportable_attributes.as_json
+      expect(attributes["details"]).not_to have_key("nation_applicability")
+    end
+
+    it "returns a list of applicable nations if there are inapplicable nations" do
+      policy_area = FactoryGirl.create(
+        :policy_area,
+        northern_ireland: false,
+        scotland: false,
+        wales: false,
+      )
+
+      attributes = ContentItemPresenter.new(policy_area).exportable_attributes.as_json
+      nation_applicability = attributes["details"]["nation_applicability"]
+      expect(nation_applicability["applies_to"]).to eq(["england"])
+      expect(nation_applicability["alternative_policies"].length).to eq(0)
+    end
+
+    it "returns a list of alternative policies for other nations if there are alternative policies" do
+      policy_area = FactoryGirl.create(
+        :policy_area,
+        northern_ireland: false,
+        northern_ireland_policy_url: "https://www.example.ni",
+        scotland: false,
+        scotland_policy_url: "http://www.example.scot",
+        wales: false,
+        wales_policy_url: "http://example.wales",
+      )
+
+      attributes = ContentItemPresenter.new(policy_area).exportable_attributes.as_json
+      nation_applicability = attributes["details"]["nation_applicability"]
+      expect(nation_applicability["applies_to"]).to eq(["england"])
+      expect(nation_applicability["alternative_policies"].length).to eq(3)
+      expect(nation_applicability["alternative_policies"].first["nation"]).to eq("northern_ireland")
+      expect(nation_applicability["alternative_policies"].first["alt_policy_url"]).to eq("https://www.example.ni")
+    end
   end
 end
