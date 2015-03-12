789950a2b17c2d19332bfa428e2cd3a31fde5f68
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-03-12 16:26:26 +0000

    Editors can set applicable nations
    This commit adds a shared view for setting applicable nations. It also
    contains some JS which will only display the alternative policy URL if
    that nation's checkbox is unchecked.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,8 @@
 
 $(function () {
   $(".select2").select2();
+
+  $(".applicable-nations input[type='checkbox']").click(function() {
+    $('.js-alternative-policy-url#' + $(this).data('nation')).toggle();
+  });
 });
--- app/models/concerns/applicable_to_nations.rb
+++ app/models/concerns/applicable_to_nations.rb
@@ -1,6 +1,10 @@
 module ApplicableToNations
   extend ActiveSupport::Concern
 
+  included do
+    validate :applicable_to_at_least_one_nation
+  end
+
   def possible_nations
     [
       :england,
@@ -14,5 +18,13 @@ module ApplicableToNations
     applicable_nations = possible_nations.select { |n|
       self.send(n) == true
     }
+  end
+
+private
+
+  def applicable_to_at_least_one_nation
+    if applicable_nations.empty?
+      errors.add(:applicability, "must have at least one nation")
+    end
   end
 end
