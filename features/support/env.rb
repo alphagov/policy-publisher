ad14c768d9921049efa7e7deb752c0c20df4e964
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-03-19 13:58:38 +0000

    Add poltergeist
    Poltergeist is needed as we now need to test using javascript in the
    features.

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,6 +10,9 @@ After('@no-txn') { DatabaseCleaner.strategy = :transaction }
 require 'cucumber/rails'
 require 'cucumber/rspec/doubles'
 
+require "capybara/poltergeist"
+Capybara.javascript_driver = :poltergeist
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
