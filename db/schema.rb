4d5b069174dd5108939356d78ddf093c1a652cd1
Tekin Suleyman <tekin@tekin.co.uk>
2015-04-16 10:19:06 +0100

    Seed new policies data
    This loads the opening gambit of policies into policy publisher. It seeds the
    policies based on their new titles, their existing organisation mappings and
    content_id. The second migration then creates the association between the four
    known programmes and their parent policy areas.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413160915) do
+ActiveRecord::Schema.define(version: 20150416085800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
