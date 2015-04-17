c1d3de8c595cb100110ecc18d8a8023fad1d10fc
Tekin Suleyman <tekin@tekin.co.uk>
2015-04-17 11:47:11 +0100

    Rewrite wiring for Publisher
    Now we have a command object for each action to be performed, we can wire them
    altogether in a manner which makes things much easier to change and configure.

--- lib/publisher.rb
+++ lib/publisher.rb
@@ -1,27 +1,18 @@
-# Publisher for publishing polices onto GOV.UK
+# Publisher for publishing a policy live onto GOV.UK
 class Publisher
   attr_reader :policy
+
   def initialize(policy)
     @policy = policy
   end
 
   def publish!
-    publish_to_publishing_api!
-    add_to_search_index!
-    republish_parents_to_publishing_api!
+    publish_actions.each { |klass| klass.new(policy).run! }
   end
 
 private
 
-  def publish_to_publishing_api!
-    ContentItemPublisher.new(policy).run!
-  end
-
-  def republish_parents_to_publishing_api!
-    ParentPolicyContentItemRepublisher.new(policy).run!
-  end
-
-  def add_to_search_index!
-    SearchIndexer.new(policy).run!
+  def publish_actions
+    [ContentItemPublisher, ParentPolicyContentItemRepublisher, SearchIndexer]
   end
 end
