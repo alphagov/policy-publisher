1a35b33489c8a03ecbb099b637c5f4dbee0e8332
Tekin Suleyman <tekin@tekin.co.uk>
2015-04-20 10:33:32 +0100

    Add additional comments to Publisher
    This is mainly to highlight that this class is introducing some tech debt that
    will eventually be paid down when the publishing stack supports the stuff we
    need.

--- lib/publisher.rb
+++ lib/publisher.rb
@@ -8,6 +8,15 @@
 #   * Re-publishing any parent policies to the publshing API to keep the
 #     "related" policies of the parents up-to-date.
 #
+# Note: This class is dealing with a bunch of stuff that would ideally be
+# handled by other parts of the new publishing stack if they existed. e.g. a
+# search indexing service would index the content for search; a dependency
+# resolver would send out a notification to indicate that the parent policies
+# should be republish; another service would generate the content item for the
+# email subscription. These will all be added in the fullness of time, at which
+# point this class becomes redundant and ContentItemPublisher can be used
+# instead.
+#
 class Publisher
   attr_reader :policy
 
