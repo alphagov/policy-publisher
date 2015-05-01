c60e684d41f9a419161b74677aba5b82173fb18a
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-05-01 11:13:14 +0100

    Remove feature flags from feature tests

--- features/support/future_policies.rb
+++
@@ -1,5 +0,0 @@
-# Turn on temporary feature-flag for future-policies feature during selected tests
-# As it's stored in the database it will be wiped after every test anyway
-Before("@future-policies") do
-  FeatureFlag.create(key: 'future_policies', enabled: true)
-end
