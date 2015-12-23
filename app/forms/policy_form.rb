1374c52001f827f4ebacd1469c62a0226e05d3ff
Tijmen Brommet <tijmen@gmail.com>
2015-12-23 11:41:27 +0000

    Refactor policy update/create into form object
    We are going to need this form object, because some the link-attributes
    will soon come from the publishing-api instead of the database. By
    adding an intermediate layer, we can still keep the same interface in
    the views and controllers, but send/fetch the values from somewhere
    else.

---
+++ app/forms/policy_form.rb
@@ -0,0 +1,88 @@
+class PolicyForm
+  ATTRIBUTES = %w[
+    id
+    name
+    description
+    sub_policy
+    england
+    england_policy_url
+    northern_ireland
+    northern_ireland_policy_url
+    scotland
+    scotland_policy_url
+    wales
+    wales_policy_url
+    organisation_content_ids
+    people_content_ids
+    working_group_content_ids
+    parent_policy_ids
+  ]
+
+  attr_accessor *ATTRIBUTES
+  attr_accessor :policy
+
+  include ActiveModel::Model
+
+  def self.model_name
+    ActiveModel::Name.new(Policy)
+  end
+
+  def self.new_with_defaults
+    new(
+      england: true,
+      scotland: true,
+      wales: true,
+      northern_ireland: true,
+      organisation_content_ids: [],
+      people_content_ids: [],
+      working_group_content_ids: [],
+      parent_policy_ids: [],
+    )
+  end
+
+  def self.from_form(policy_params)
+    new(policy_params)
+  end
+
+  def self.from_existing(policy)
+    form = new(policy.as_json.slice(*ATTRIBUTES))
+    form.policy = policy
+    form
+  end
+
+  def update(attrs)
+    attrs.each { |k, v| self.send("#{k}=", v) }
+    save
+  end
+
+  def save
+    attributes = as_json.slice(*ATTRIBUTES)
+    policy.update_attributes(attributes) && publish!
+  end
+
+  def error_message
+    policy.errors.full_messages.to_sentence.downcase
+  end
+
+  def policy
+    @policy ||= Policy.new
+  end
+
+  def persisted?
+    policy.id.present?
+  end
+
+  def possible_nations
+    policy.possible_nations
+  end
+
+  def sub_policy?
+    sub_policy
+  end
+
+private
+  def publish!
+    Publisher.new(policy).publish!
+    true
+  end
+end
