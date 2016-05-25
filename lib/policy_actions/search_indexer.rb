b14cd6ae7e64b4d4418532772b7435077f821f4a
Tijmen Brommet <tijmen@gmail.com>
2016-05-25 15:48:34 +0100

    Move tagging lookup to SearchIndexer
    To send the proper payload to Rummager, we need to look up the base
    path for the items linked to the policy. Currently this happens in the
    model. By moving it out to the `SearchIndexer` we can make the model
    cleaner and make the code more clear.
    
    The removed unit tests are covered by the tests for the search indexer.

--- lib/policy_actions/search_indexer.rb
+++ lib/policy_actions/search_indexer.rb
@@ -17,15 +17,35 @@ class SearchIndexer
       link: policy.base_path,
       slug: policy.slug,
       indexable_content: "",
-      organisations: get_slugs(policy.organisations),
-      people: get_slugs(policy.people),
-      policy_groups: get_slugs(policy.working_groups),
+      organisations: organisations,
+      people: people,
+      policy_groups: working_groups,
       public_timestamp: policy.updated_at,
     }
   end
 
 private
+
+  def organisations
+    fetched_organisations = policy.organisation_content_ids.map { |content_id| fetcher.find_organisation(content_id) }.compact
+    get_slugs(fetched_organisations)
+  end
+
+  def people
+    fetched_people = policy.people_content_ids.map { |content_id| fetcher.find_person(content_id) }.compact
+    get_slugs(fetched_people)
+  end
+
+  def working_groups
+    fetched_working_groups = policy.working_group_content_ids.map { |content_id| fetcher.find_working_group(content_id) }.compact
+    get_slugs(fetched_working_groups)
+  end
+
   def get_slugs(items)
     items.map { |cri| cri["base_path"].gsub(%r{.*/([^/]+?)$}, '\1') }
+  end
+
+  def fetcher
+    @fetcher ||= ContentItemFetcher.new
   end
 end
