396dd62b0885b6dddaf74b9911ea6206217357c8
Tijmen Brommet <tijmen@gmail.com>
2016-06-01 17:57:55 +0100

    Remove redundant policy
    This withdraws a redundant policy from the publishing_api. I’ve chosen
    to `discard_drafts` because it’s not coming back.
    
    Related zendesk:
    
    https://govuk.zendesk.com/agent/tickets/1299583

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525154825) do
+ActiveRecord::Schema.define(version: 20160601111826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
