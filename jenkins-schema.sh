b74e40b56c6ece8427e5b264e24e16efb20cf731
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-01-11 11:54:50 +0000

    Delete old Jenkins shell scripts
    The contents of these scripts have been migrated to the Jenkinsfile.
    
    These were not safe to delete until now because they were still used to
    test the content schemas on Jenkins 1, but those tests have now also
    been migrated to Jenkins 2.

--- jenkins-schema.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-export GOVUK_APP_DOMAIN=test.alphagov.co.uk
-export GOVUK_ASSET_ROOT=http://static.test.alphagov.co.uk
-export REPO_NAME="alphagov/govuk-content-schemas"
-export CONTEXT_MESSAGE="Verify policy-publisher against content schemas"
-
-exec ./jenkins.sh
