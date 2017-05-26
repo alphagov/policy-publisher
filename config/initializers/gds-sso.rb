3629fefbeff4d692db98a894e26cff6de2b1994f
Tijmen Brommet <tijmen@gmail.com>
2017-05-26 14:23:31 +0100

    Use environment variables to configure application
    We're making this app a 12 factor application:
    
    https://trello.com/c/wf4VYuDO/119-12-factorise-policy-publisher

--- config/initializers/gds-sso.rb
+++
@@ -1,8 +0,0 @@
-# This initialiser is overwritten when deployed to integration, staging & production.
-
-GDS::SSO.config do |config|
-  config.user_model   = 'User'
-  config.oauth_id     = ENV['OAUTH_ID'] || "abcdefg"
-  config.oauth_secret = ENV['OAUTH_SECRET'] || "secret"
-  config.oauth_root_url = Plek.current.find('signon')
-end
