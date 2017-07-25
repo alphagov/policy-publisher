9d7b71c83eef2296d7af178f4ac7d882a68634a3
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-07-25 09:10:18 +0100

    Stop sending update_type of major or minor on publish
    Sending it on publish has been deprecated in favour of including
    it when sending a put content.

--- lib/policy_firehose_finder_publisher.rb
+++ lib/policy_firehose_finder_publisher.rb
@@ -13,7 +13,7 @@ class PolicyFirehoseFinderPublisher
 
   def publish
     publishing_api.put_content(CONTENT_ID, exportable_attributes)
-    publishing_api.publish(CONTENT_ID, 'major')
+    publishing_api.publish(CONTENT_ID)
   end
 
   def exportable_attributes
